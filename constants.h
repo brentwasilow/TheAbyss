798c2cdd8e3dceb08cdd77f2c53353afde6511d0
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-25 03:59:10 -0700

    header/namespace filled with necessary game constants

---
+++ constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H_INCLUDED__
+#define __CONSTANTS_H_INCLUDED__
+
+namespace Constants {
+    const int WIDTH = 640;
+    const int HEIGHT = 480;
+    const char* TITLE = "The Abyss";
+    const int ANTI_ALIASING_LEVEL = 8;
+    const double FPS = 60.0;
+    const double UPS = 60.0;
+    const double FOV = 60.0;
+}
+
+#endif
