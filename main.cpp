1fb7105d59b7e417abe04fb852f693a6963524cf
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-25 04:00:42 -0700

    Modified main to use constants from the newly created constants.h namespace

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
-
-#define WIDTH 640
-#define HEIGHT 480
-#define TITLE "The Abyss"
-#define ANTI_ALIASING_LEVEL 8
-#define FPS 60
+#include "constants.h"
 
 int main(int argc, char* argv[]) {
     // context settings for window
     sf::ContextSettings settings;
-    settings.antialiasingLevel = ANTI_ALIASING_LEVEL;
+    settings.antialiasingLevel = Constants::ANTI_ALIASING_LEVEL;
 
     // create a window
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), TITLE, sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(Constants::WIDTH, Constants::HEIGHT), Constants::TITLE, sf::Style::Default, settings);
 
     // set window properties
-    window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width/2)-(WIDTH/2), (sf::VideoMode::getDesktopMode().height/2)-(HEIGHT/2)));
-    window.setFramerateLimit(FPS);
+    window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width/2)-(Constants::WIDTH/2), (sf::VideoMode::getDesktopMode().height/2)-(Constants::HEIGHT/2)));
+    window.setFramerateLimit(Constants::FPS);
 
     // create ceiling and floor background shapes/colors/positions
-    sf::RectangleShape floor(sf::Vector2f(WIDTH, HEIGHT/2));
+    sf::RectangleShape floor(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
     floor.setFillColor(sf::Color(50, 50, 50));
     floor.setPosition(0, 0);
 
-    sf::RectangleShape ceiling(sf::Vector2f(WIDTH, HEIGHT/2));
+    sf::RectangleShape ceiling(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
     ceiling.setFillColor(sf::Color(100, 100, 100));
-    floor.setPosition(0, HEIGHT/2);
+    floor.setPosition(0, Constants::HEIGHT/2);
 
 
     // run the program as long as the window is open
