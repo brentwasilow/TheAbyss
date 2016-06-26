30ffd9dfe3da537f76827444203d03314df3b3af
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-25 23:22:32 -0700

    render header file that contains initialize method to setup window and background properties and draw background method

---
+++ include/render.h
@@ -0,0 +1,13 @@
+#ifndef __RENDER_H_INCLUDED__
+#define __RENDER_H_INCLUDED__
+
+#include <SFML/Graphics.hpp>
+
+namespace Render {
+
+void init(sf::RenderWindow&);
+void drawBackground(sf::RenderWindow&);
+
+};
+
+#endif
