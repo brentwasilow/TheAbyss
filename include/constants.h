9b8a5d002a154861a64c62ef8183be134844bf75
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-25 23:20:35 -0700

    modifed constants namespace to fix linking issues

--- include/constants.h
+++ include/constants.h
@@ -2,13 +2,13 @@
 #define __CONSTANTS_H_INCLUDED__
 
 namespace Constants {
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
-    const char* TITLE = "The Abyss";
-    const int ANTI_ALIASING_LEVEL = 8;
-    const double FPS = 60.0;
-    const double UPS = 60.0;
-    const double FOV = 60.0;
-}
+    static const int WIDTH = 640;
+    static const int HEIGHT = 480;
+    static const char* TITLE = "The Abyss";
+    static const int ANTI_ALIASING_LEVEL = 8;
+    static const double FPS = 60.0;
+    static const double UPS = 60.0;
+    static const double FOV = 60.0;
+};
 
 #endif
