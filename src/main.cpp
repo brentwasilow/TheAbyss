4b2f383b7416c14db846baf137b9b7b272e00041
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-25 23:24:34 -0700

    modified main method to reflect new render header/source file changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
 #include <SFML/Graphics.hpp>
-#include "constants.h"
+#include "render.h"
 
 int main(int argc, char* argv[]) {
-    // context settings for window
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = Constants::ANTI_ALIASING_LEVEL;
-
-    // create a window
-    sf::RenderWindow window(sf::VideoMode(Constants::WIDTH, Constants::HEIGHT), Constants::TITLE, sf::Style::Default, settings);
-
-    // set window properties
-    window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width/2)-(Constants::WIDTH/2), (sf::VideoMode::getDesktopMode().height/2)-(Constants::HEIGHT/2)));
-    window.setFramerateLimit(Constants::FPS);
-
-    // create ceiling and floor background shapes/colors/positions
-    sf::RectangleShape floor(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
-    floor.setFillColor(sf::Color(50, 50, 50));
-    floor.setPosition(0, 0);
-
-    sf::RectangleShape ceiling(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
-    ceiling.setFillColor(sf::Color(100, 100, 100));
-    floor.setPosition(0, Constants::HEIGHT/2);
-
+    // create and initialize window
+    sf::RenderWindow window;
+    Render::init(window);
 
     // run the program as long as the window is open
     while (window.isOpen()) {
@@ -37,8 +20,7 @@ int main(int argc, char* argv[]) {
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        window.draw(floor);
-        window.draw(ceiling);
+        Render::drawBackground(window);
 
         // end the current frame
         window.display();
