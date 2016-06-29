03b59a2766ac82db3df4281f1b0a13164389d565
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-28 20:03:51 -0700

    removed title song variable

--- include/render.h
+++ include/render.h
@@ -2,10 +2,8 @@
 #define __RENDER_H_INCLUDED__
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 
 namespace Render {
-    static sf::Music titleSong;
     void init(sf::RenderWindow&);
     void drawTitleScreen(sf::RenderWindow&);
     void drawBackground(sf::RenderWindow&);
