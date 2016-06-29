9bd47c0a1d06adf01b6a7439ae6199e50e57dbb8
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-28 23:17:39 -0700

    added Player structure reference for drawing map

--- src/render.cpp
+++ src/render.cpp
@@ -1,15 +1,22 @@
 #include "render.h"
 #include "constants.h"
 #include <cmath>
-#include <iostream>
-#define _USE_MATH_DEFINES
 
 static sf::RectangleShape backgroundFloor;
 static sf::RectangleShape backgroundCeiling;
 static sf::Text titleText;
 static sf::Font font;
 
-void Render::init(sf::RenderWindow& window) {
+double verticalIntersection(double, Player&);
+double horizontalIntersection(double, Player&);
+
+int map[5][5] = {{1, 1, 1, 1, 1},
+                 {1, 0, 0, 0, 1},
+                 {1, 0, 0, 0, 1},
+                 {1, 0, 0, 0, 1},
+                 {1, 1, 1, 1, 1}};
+
+void Render::initialize(sf::RenderWindow& window) {
     // context settings for window
     sf::ContextSettings settings;
     settings.antialiasingLevel = Constants::ANTI_ALIASING_LEVEL;
@@ -18,15 +25,16 @@ void Render::init(sf::RenderWindow& window) {
     window.create(sf::VideoMode(Constants::WIDTH, Constants::HEIGHT), Constants::TITLE, sf::Style::Default, settings);
 
     // set window properties
-    window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width/2)-(Constants::WIDTH/2), (sf::VideoMode::getDesktopMode().height/2)-(Constants::HEIGHT/2)));
+    sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+    window.setPosition(sf::Vector2i((mode.width/2)-Constants::WIDTH_2, (mode.height/2)-(Constants::HEIGHT_2)));
     window.setFramerateLimit(Constants::FPS);
 
     // create floor and ceiling background sizes/colors/positions
-    backgroundFloor.setSize(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
+    backgroundFloor.setSize(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT_2));
     backgroundFloor.setFillColor(sf::Color(50, 50, 50));
-    backgroundFloor.setPosition(0, Constants::HEIGHT/2);
+    backgroundFloor.setPosition(0, Constants::HEIGHT_2);
 
-    backgroundCeiling.setSize(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT/2));
+    backgroundCeiling.setSize(sf::Vector2f(Constants::WIDTH, Constants::HEIGHT_2));
     backgroundCeiling.setFillColor(sf::Color(100, 100, 100));
     backgroundCeiling.setPosition(0, 0);
 
@@ -41,34 +49,23 @@ void Render::init(sf::RenderWindow& window) {
     sf::FloatRect textRect = titleText.getLocalBounds();
     titleText.setOrigin(textRect.left + textRect.width/2.0f,
                         textRect.top  + textRect.height/2.0f);
-    titleText.setPosition(sf::Vector2f(Constants::WIDTH/2.0f, Constants::HEIGHT/2.0f));
+    titleText.setPosition(sf::Vector2f(Constants::WIDTH_2, Constants::HEIGHT_2));
 }
 
 void Render::drawTitleScreen(sf::RenderWindow& window) {
     window.draw(titleText);
 }
 
-int map[5][5] = {{1, 1, 1, 1, 1},
-                 {1, 0, 0, 0, 1},
-                 {1, 0, 0, 0, 1},
-                 {1, 0, 0, 0, 1},
-                 {1, 1, 1, 1, 1}};
-
-double viewingAngle = 90.0;
-double angleBetweenRays = Constants::FOV / (double)(Constants::WIDTH);
-int tileSize = 64.0;
-
-void Render::drawMap(sf::RenderWindow& window) {
-    double fovRadians = ((Constants::FOV/2.0)*M_PI/180.0);
-    double distanceToProjection = Constants::WIDTH/ 2.0 / tan(fovRadians);
-    double angle = viewingAngle + (Constants::FOV/2.0);
+void Render::drawMap(sf::RenderWindow& window, Player& player) {
+    double distanceToProjection = Constants::WIDTH_2 / tan(Constants::FOV_2_R);
+    double angle = player.angle + Constants::FOV_2_D;
 
     for (int x = 0; x < Constants::WIDTH; x++) {
         if (angle < 0.0) angle += 360.0;
         else if (angle >= 360.0) angle -= 360.0;
 
-        double verticalDistance = Render::verticalIntersection(angle);
-        double horizontalDistance = Render::horizontalIntersection(angle);
+        double verticalDistance = verticalIntersection(angle, player);
+        double horizontalDistance = horizontalIntersection(angle, player);
 
         double distance;
         if (horizontalDistance < verticalDistance) {
@@ -76,24 +73,21 @@ void Render::drawMap(sf::RenderWindow& window) {
         } else {
             distance = verticalDistance;
         }
-        double correctDistance = distance * cos((angle - viewingAngle) * M_PI / 180.0);
-        double projectedSliceHeight = distanceToProjection * tileSize / correctDistance;
+        double correctDistance = distance * cos((angle - player.angle) * M_PI / 180.0);
+        double projectedSliceHeight = distanceToProjection * Constants::TILE_SIZE / correctDistance;
 
         sf::RectangleShape wall;
         wall.setSize(sf::Vector2f(1.0, projectedSliceHeight));
         wall.setFillColor(sf::Color(255, 0, 255));
-        wall.setPosition(x, (Constants::HEIGHT/2.0)-(projectedSliceHeight/2.0));
+        wall.setPosition(x, (Constants::HEIGHT_2)-(projectedSliceHeight/2.0));
 
         window.draw(wall);
 
-        angle -= angleBetweenRays;
+        angle -= Constants::ANGLE_BETWEEN_RAYS;
     }
 }
 
-double x = 160.0;
-double y = 160.0;
-
-double Render::verticalIntersection(double angle) {
+double verticalIntersection(double angle, Player& player) {
     double verticalX, verticalY;
     double dx, dy;
     double t = tan(angle * M_PI / 180.0);
@@ -101,19 +95,19 @@ double Render::verticalIntersection(double angle) {
     if (angle == 90.0 || angle == 270.0) return 1000000.0;
 
     if (angle > 90.0 && angle < 270.0) {
-        verticalX = (x / tileSize) * tileSize;
-        dx = -tileSize;
-        verticalY = y + ((x - verticalX) * t);
-        dy = t * tileSize;
+        verticalX = (player.x / Constants::TILE_SIZE) * Constants::TILE_SIZE;
+        dx = -Constants::TILE_SIZE;
+        verticalY = player.y + ((player.x - verticalX) * t);
+        dy = t * Constants::TILE_SIZE;
         verticalX--;
     } else {
-        verticalX = ((x / tileSize) * tileSize) + tileSize;
-        dx = tileSize;
-        verticalY = y + ((x - verticalX) * t);
-        dy = -t * tileSize;
+        verticalX = ((player.x / Constants::TILE_SIZE) * Constants::TILE_SIZE) + Constants::TILE_SIZE;
+        dx = Constants::TILE_SIZE;
+        verticalY = player.y + ((player.x - verticalX) * t);
+        dy = -t * Constants::TILE_SIZE;
     }
-    int column = (int) (verticalX / tileSize);
-    int row = (int) (verticalY / tileSize);
+    int column = (int) (verticalX / Constants::TILE_SIZE);
+    int row = (int) (verticalY / Constants::TILE_SIZE);
 
     if (row < 0 || row >= 5 || column < 0 || column >= 5) return 1000000.0;
 
@@ -121,16 +115,17 @@ double Render::verticalIntersection(double angle) {
         verticalX += dx;
         verticalY += dy;
 
-        column = (int) (verticalX / tileSize);
-        row = (int) (verticalY / tileSize);
+        column = (int) (verticalX / Constants::TILE_SIZE);
+        row = (int) (verticalY / Constants::TILE_SIZE);
 
         if (row < 0 || row >= 5 || column < 0 || column >= 5) return 1000000.0;
     }
-    double tempDistance = ((verticalX-x)*(verticalX-x))+((verticalY-y)*(verticalY-y));
+    double tempDistance = ((verticalX-player.x)*(verticalX-player.x))+
+                          ((verticalY-player.y)*(verticalY-player.y));
     return sqrt(tempDistance);
 }
 
-double Render::horizontalIntersection(double angle) {
+double horizontalIntersection(double angle, Player& player) {
     double horizontalX, horizontalY;
     double dx, dy;
     double t = tan(angle * M_PI / 180.0);
@@ -139,31 +134,31 @@ double Render::horizontalIntersection(double angle) {
     if (angle == 0.0 || angle == 180.0) return 1000000.0;
 
     if (angle > 0.0 && angle < 180.0) {
-        horizontalY = (y / tileSize) * tileSize;
-        dy = -tileSize;
+        horizontalY = (player.y / Constants::TILE_SIZE) * Constants::TILE_SIZE;
+        dy = -Constants::TILE_SIZE;
 
         if (angle == 90.0) {
-            horizontalX = x;
+            horizontalX = player.x;
             dx = 0.0;
         } else {
-            horizontalX = x + ((y - horizontalY) / t);
-            dx = tileSize / t;
+            horizontalX = player.x + ((player.y - horizontalY) / t);
+            dx = Constants::TILE_SIZE / t;
         }
         horizontalY--;
     } else {
-        horizontalY = ((y/tileSize)*tileSize) + tileSize;
-        dy = tileSize;
+        horizontalY = ((player.y/Constants::TILE_SIZE)*Constants::TILE_SIZE) + Constants::TILE_SIZE;
+        dy = Constants::TILE_SIZE;
 
         if (angle == 270.0) {
-            horizontalX = x;
+            horizontalX = player.x;
             dx = 0.0;
         } else {
-            horizontalX = x - ((y - horizontalY) / it);
-            dx = tileSize / it;
+            horizontalX = player.x - ((player.y - horizontalY) / it);
+            dx = Constants::TILE_SIZE / it;
         }
     }
-    int column = (int) (horizontalX / tileSize);
-    int row = (int) (horizontalY / tileSize);
+    int column = (int) (horizontalX / Constants::TILE_SIZE);
+    int row = (int) (horizontalY / Constants::TILE_SIZE);
 
     if (row < 0 || row >= 5 || column < 0 || column >= 5) return 1000000.0;
 
@@ -171,12 +166,13 @@ double Render::horizontalIntersection(double angle) {
         horizontalX += dx;
         horizontalY += dy;
 
-        column = (int) (horizontalX / tileSize);
-        row = (int) (horizontalY / tileSize);
+        column = (int) (horizontalX / Constants::TILE_SIZE);
+        row = (int) (horizontalY / Constants::TILE_SIZE);
 
         if (row < 0 || row >= 5 || column < 0 || column >= 5) return 1000000.0;
     }
-    double tempDistance = ((horizontalX-x)*(horizontalX-x))+((horizontalY-y)*(horizontalY-y));
+    double tempDistance = ((horizontalX-player.x)*(horizontalX-player.x))+
+                          ((horizontalY-player.y)*(horizontalY-player.y));
     return sqrt(tempDistance);
 }
 
