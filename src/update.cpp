fc55a56af2de7cc7606b65da71a70aa08cc16348
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-28 23:19:31 -0700

    defining functionality for movement function using the SFML keyboard class

---
+++ src/update.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include "update.h"
+
+static double speed = 5.0;
+
+void Update::movement() {
+
+    int row = 0, column = 0;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        
+    }
+}
