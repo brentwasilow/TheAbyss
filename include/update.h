9dabbbb9f556fe5fa7fe9759d375da1a3d085d28
Brent Wasilow <brentwasilow2@gmail.com>
2016-06-29 23:54:52 -0700

    added definitions for updating window closing state and title to game transition

--- include/update.h
+++ include/update.h
@@ -1,8 +1,15 @@
 #ifndef __UPDATE_H_INCLUDED__
 #define __UPDATE_H_INCLUDED__
 
+#include <SFML/Graphics.hpp>
+#include "player.h"
+
 namespace Update {
-    void movement();
+    extern int state;
+
+    void checkWindowState(sf::RenderWindow&);
+    void checkTitleToGameState();
+    void checkMovement(Player&);
 }
 
 #endif
