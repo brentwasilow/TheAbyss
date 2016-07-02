cccc0d83cab4fd2f12607bb70d0eeb9e909c5401
Brent Wasilow <brentwasilow2@gmail.com>
2016-07-02 14:56:48 -0700

    removed level loading steps

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "player.h"
 #include "update.h"
 #include "constants.h"
+#include "level.h"
 
 void update(sf::RenderWindow&, Player&);
 void render(sf::RenderWindow&, Player&);
@@ -19,32 +20,9 @@ int main(int argc, char* argv[]) {
     player.angle = 90.0;
     player.speed = 5.0;
 
-    // load level
-    sf::Image level;
-    if (!level.loadFromFile("res/level1.png")) {
-        return -1;
-    }
+    // load map
+    std::vector<std::vector<int> > map = Level::loadMap("res/level1.png");
 
-    int map[level.getSize().y][level.getSize().x];
-    for (uint y = 0; y < level.getSize().y; y++) {
-        for (uint x = 0; x < level.getSize().x; x++) {
-            sf::Color color = level.getPixel(x, y);
-
-            if (color == sf::Color::White) {
-                map[y][x] = Constants::EMPTY;
-            } else if (color == sf::Color::Black) {
-                map[y][x] = Constants::BRICK;
-            } else if (color == sf::Color::Blue) {
-                map[y][x] = Constants::FLAG;
-            } else if (color == sf::Color::Red) {
-                player.x = (x * Constants::TILE_SIZE) + Constants::TILE_SIZE/2;
-                player.y = (y * Constants::TILE_SIZE) + Constants::TILE_SIZE/2;
-                map[y][x] = Constants::EMPTY;
-            } else {
-
-            }
-        }
-    }
 
     // define clock and ancillaries for game loop
     sf::Clock clock;
