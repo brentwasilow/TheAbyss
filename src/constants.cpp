4ac28f5b39a8d1061fe310ddd8fb1486c1a9f2a4
Brent Wasilow <brentwasilow2@gmail.com>
2016-07-07 03:38:04 -0700

    style changes

--- src/constants.cpp
+++ src/constants.cpp
@@ -5,9 +5,9 @@
 
 namespace Constants {
     const int WIDTH = 640;
-    const int WIDTH_2 = WIDTH/2;
+    const int WIDTH_2 = (WIDTH/2);
     const int HEIGHT = 480;
-    const int HEIGHT_2 = HEIGHT/2;
+    const int HEIGHT_2 = (HEIGHT/2);
 
     const char* TITLE = "The Abyss";
     const int ANTI_ALIASING_LEVEL = 8;
@@ -16,16 +16,16 @@ namespace Constants {
     const double UPS = 60.0;
 
     const double FOV_D = 60.0;
-    const double FOV_R = (FOV_D * M_PI) / 180.0;
-    const double FOV_2_D = FOV_D/2;
-    const double FOV_2_R = (FOV_2_D * M_PI) / 180.0;
+    const double FOV_R = FOV_D * (M_PI/180.0);
+    const double FOV_2_D = (FOV_D/2);
+    const double FOV_2_R = FOV_2_D * (M_PI/180.0);
 
-    const int DISTANCE_TO_PROJECTION = WIDTH_2/tan(FOV_2_R);
-    const double ANGLE_BETWEEN_RAYS = FOV_D/WIDTH;
+    const int DISTANCE_TO_PROJECTION = (WIDTH_2/tan(FOV_2_R));
+    const double ANGLE_BETWEEN_RAYS = (FOV_D/WIDTH);
     const int TILE_SIZE = 64;
 
     const int TICKS_PER_SECOND = 60;
-    const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+    const int SKIP_TICKS = (1000/TICKS_PER_SECOND);
     const int MAX_FRAMESKIP = 10;
 
     const int EMPTY = 0;
