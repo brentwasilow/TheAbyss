80d9060e27bcb477f142c2db3dd6903df3a837cd
Brent Wasilow <brentwasilow2@gmail.com>
2016-08-26 20:29:36 -0700

    modified global constants

--- include/constants.h
+++ include/constants.h
@@ -1,8 +1,6 @@
 #ifndef __CONSTANTS_H_INCLUDED__
 #define __CONSTANTS_H_INCLUDED__
 
-#include <string>
-
 namespace Constants {
     extern const int WIDTH;
     extern const int WIDTH_2;
@@ -12,29 +10,26 @@ namespace Constants {
     extern const char* TITLE;
     extern const int ANTI_ALIASING_LEVEL;
 
-    extern const double FPS;
-    extern const double UPS;
-
     extern const double FOV_D;
     extern const double FOV_R;
     extern const double FOV_2_D;
     extern const double FOV_2_R;
 
-    extern const int DISTANCE_TO_PROJECTION;
-    extern const double ANGLE_BETWEEN_RAYS;
     extern const int TILE_SIZE;
     extern const int TILE_SIZE_2;
+    extern const int TILE_SIZE_MASK;
+    extern const int TILE_SIZE_MASK_2;
+
+    extern const int DISTANCE_TO_PROJECTION;
     extern const int DISTANCE_TO_PROJECTION_TILE_SIZE;
 
     extern const int TICKS_PER_SECOND;
     extern const int SKIP_TICKS;
     extern const int MAX_FRAMESKIP;
 
-    extern const double DOOR_OPEN_TIME;
-
-    extern const std::string DELIM;
-
     extern const int PIXEL_BUFFER_SIZE;
+
+    extern const double DOOR_OPEN_TIME;
 };
 
 #endif
--- src/constants.cpp
+++ src/constants.cpp
@@ -12,27 +12,24 @@ namespace Constants {
     const char* TITLE = "The Abyss";
     const int ANTI_ALIASING_LEVEL = 8;
 
-    const double FPS = 60.0;
-    const double UPS = 60.0;
-
     const double FOV_D = 60.0;
     const double FOV_R = FOV_D * (M_PI/180.0);
     const double FOV_2_D = (FOV_D/2);
     const double FOV_2_R = FOV_2_D * (M_PI/180.0);
 
-    const int DISTANCE_TO_PROJECTION = (WIDTH_2/tan(FOV_2_R));
-    const double ANGLE_BETWEEN_RAYS = (FOV_D/WIDTH);
     const int TILE_SIZE = 64;
-    const int TILE_SIZE_2 = TILE_SIZE/2;
+    const int TILE_SIZE_2 = (TILE_SIZE/2);
+    const int TILE_SIZE_MASK = log2(TILE_SIZE);
+    const int TILE_SIZE_MASK_2 = log2(TILE_SIZE_2);
+
+    const int DISTANCE_TO_PROJECTION = (WIDTH_2/tan(FOV_2_R));
     const int DISTANCE_TO_PROJECTION_TILE_SIZE = DISTANCE_TO_PROJECTION * TILE_SIZE;
 
     const int TICKS_PER_SECOND = 60;
     const int SKIP_TICKS = (1000/TICKS_PER_SECOND);
     const int MAX_FRAMESKIP = 10;
 
-    const double DOOR_OPEN_TIME = 300.0;
-
-    const std::string DELIM = ":=";
-
     const int PIXEL_BUFFER_SIZE = (WIDTH * HEIGHT * 4);
+
+    const double DOOR_OPEN_TIME = 300.0;
 };
